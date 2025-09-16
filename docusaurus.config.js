b7a18e5be22afce3ce5ca3a4eb52c475c95a5b42
FvxkYqnx <wisk3138@gmail.com>
2025-09-16 09:01:06 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,7 +52,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/SkeldriaX/SkeldriaWebsite/',
+            'https://github.com/SkeldriaX/SkeldriaWebsite/tree/main',
         },
         blog: {
           showReadingTime: true,
