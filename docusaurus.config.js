96313d94c8bfa4e0d20d3b9bf80ce0690b8d56ff
FvxkYqnxXD <wisk3138@gmail.com>
2025-10-02 10:37:41 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,13 +1,5 @@
-
 // @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
 import {themes as prismThemes} from 'prism-react-renderer';
-
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -15,28 +7,19 @@ const config = {
   tagline: 'Bienvenido a la documentación oficial de SkeldriaX, aquí encontraras toda la información de SkeldriaX',
   favicon: 'img/SkeldriaX.png',
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    v4: true,
   },
 
-  // Set the production url of your site here
   url: 'https://skeldriax.fvckyqnx.me',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'SkeldriaX', // Usually your GitHub org/user name.
-  projectName: 'SkeldriaWebsite', // Usually your repo name.
+  organizationName: 'SkeldriaX',
+  projectName: 'SkeldriaWebsite',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'es',
     locales: ['es'],
@@ -45,14 +28,10 @@ const config = {
   presets: [
     [
       'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/SkeldriaX/SkeldriaWebsite/tree/main',
+          editUrl: 'https://github.com/SkeldriaX/SkeldriaWebsite/tree/main',
         },
         blog: {
           showReadingTime: true,
@@ -60,106 +39,104 @@ const config = {
             type: ['rss', 'atom'],
             xslt: true,
           },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/SkeldriaX/SkeldriaWebsite/tree/main',
-          // Useful options to enforce blogging best practices
+          editUrl: 'https://github.com/SkeldriaX/SkeldriaWebsite/tree/main',
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
           onUntruncatedBlogPosts: 'warn',
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: ['./src/css/custom.css'],
         },
       }),
     ],
   ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
-      navbar: {
-        title: 'SkeldriaX',
-        logo: {
-          alt: 'SkeldriaLogo',
-          src: 'img/SkeldriaX.png',
+  themeConfig: ({
+    image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: false,
+      respectPrefersColorScheme: false,
+    },
+    navbar: {
+      title: 'SkeldriaX',
+      logo: {
+        alt: 'SkeldriaLogo',
+        src: 'img/SkeldriaX.png',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'WIKI',
         },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'WIKI',
-          },
-	  {to: '/install', label: 'Instalación', position: 'left'},
-	  {to: '/securitybot', label: 'SkeldriaSecurity', position: 'left'},
-          {
-            href: 'https://github.com/SkeldriaX/SkeldriaWebsite',
-            position: 'right',
-			className: 'navbar-icon github-icon',
-			'aria-label': 'Github',
-          },
-		  {
-			href: 'https://discord.skledirax.dev',
-			position: 'right',
-			className: 'navbar-icon discord-icon',
-			'aria-label': 'Discord',
-		  },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Introducción',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Links',
-            items: [
-              {
-                label: 'Comprar',
-                href: 'https://builtbybit.com/FvxkYqnx/',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discord.skeldriax.dev',
-              },
-              {
-                label: 'Partners',
-                href: 'https://skeldriax.dev/partners',
-              },
-            ],
-          },
-          {
-            title: 'Más',
-            items: [
-              {
-                label: 'Instalación',
-                to: '/install',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/SkeldriaX/SkeldriaWebsite',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} SkeldriaX, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
+        {to: '/install', label: 'Instalación', position: 'left'},
+        {to: '/securitybot', label: 'SkeldriaSecurity', position: 'left'},
+        {
+          href: 'https://github.com/SkeldriaX/SkeldriaWebsite',
+          position: 'right',
+          className: 'navbar-icon github-icon',
+          'aria-label': 'GitHub',
+        },
+        {
+          href: 'https://discord.skeldriax.dev',
+          position: 'right',
+          className: 'navbar-icon discord-icon',
+          'aria-label': 'Discord',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Introducción',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Links',
+          items: [
+            {
+              label: 'Comprar',
+              href: 'https://builtbybit.com/FvxkYqnx/',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discord.skeldriax.dev',
+            },
+            {
+              label: 'Partners',
+              href: 'https://skeldriax.dev/partners',
+            },
+          ],
+        },
+        {
+          title: 'Más',
+          items: [
+            {
+              label: 'Instalación',
+              to: '/install',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/SkeldriaX/SkeldriaWebsite',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} SkeldriaX, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  }),
 };
 
 export default config;
